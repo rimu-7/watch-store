bd0e2e8ac296c6adc0c5832765648d0916ecf01a
rimu-7 <rrimu99@gmail.com>
2025-03-15 22:17:08 +0800

    Footer

---
+++ app/Shared/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <p
+        className="text-neutral-700 text-sm text-center 
+        px-4 md:px-12 py-4 md:py-6"
+      >
+        Made with 🤍 by Rimu
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
