c7729fa096917a2c67d5ca087ed49de248849e32
rimu-7 <rrimu99@gmail.com>
2025-03-16 21:50:49 +0800

    navbar polishing

--- app/Shared/Navbar/Navbar.tsx
+++ app/Shared/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
-"use client" 
-import {  Search } from "lucide-react";
+"use client";
+import { Search } from "lucide-react";
 import Link from "next/link";
 
 function Navbar() {
   return (
     <nav className="px-4 md:px-12 py-4 md:py-6 ">
       <div className="flex justify-between items-center">
-        <Link href={"/"} className="hidden md:inline-block items-center">
-          BeautiWatches
+        <Link href={"/"} className="inline-block items-center">
+          Owatch
         </Link>
         <div className="relative mx-w-[300] md:max-w-[400] border-black/[0.7]">
           <div className="  absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -22,10 +22,10 @@ function Navbar() {
         </div>
         <Link href={"/add-product"}>
           <div
-            className="bg-[#212529] text-white px-3 py-2 rounded-md 
+            className="bg-[#212529] text-white text-sm px-3 py-2 rounded-md 
             cursor-pointer"
           >
-            Add Product
+            Product
           </div>
         </Link>
       </div>
